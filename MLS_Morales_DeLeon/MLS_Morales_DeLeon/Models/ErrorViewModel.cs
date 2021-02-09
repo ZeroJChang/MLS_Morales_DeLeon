e1cc691a38fe613e5d2757da465cf5aa217ccb70
ZeroJChang <jdeleonchang@gmail.com>
2021-02-08 23:04:27 -0600

    first commit

---
+++ MLS_Morales_DeLeon/MLS_Morales_DeLeon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MLS_Morales_DeLeon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
